7f9676f493c03a08af736277a677669eef2e6ed7
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-04-07 10:32:22 -0300

    refactor: changes routes in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,47 +3,47 @@ import axios from 'axios';
 const BASE_URL = 'https://back-project-mywallet.herokuapp.com';
 
 function createConfig(token) {
-	return {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+    return {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
 }
 
 async function createUser(user) {
-	await axios.post(`${BASE_URL}/signup`, user);
+    await axios.post(`${BASE_URL}/user`, user);
 }
 
 async function login(data) {
-	const token = await axios.post(`${BASE_URL}/signin`, data);
-	return token;
+    const token = await axios.post(`${BASE_URL}/`, data);
+    return token;
 }
 
 async function createRecord(token, data) {
-	const config = createConfig(token);
+    const config = createConfig(token);
 
-	await axios.post(`${BASE_URL}/newdata`, data, config);
+    await axios.post(`${BASE_URL}/mywallet`, data, config);
 }
 
 async function getUserData(token) {
-	const config = createConfig(token);
+    const config = createConfig(token);
 
-	const userRecords = await axios.get(`${BASE_URL}/mywallet`, config);
-	return userRecords;
+    const userRecords = await axios.get(`${BASE_URL}/mywallet`, config);
+    return userRecords;
 }
 
 async function logout(token) {
-	const config = createConfig(token);
+    const config = createConfig(token);
 
-	await axios.delete(`${BASE_URL}/logout`, config);
+    await axios.delete(`${BASE_URL}/logout`, config);
 }
 
 const api = {
-	createUser,
-	login,
-	createRecord,
-	getUserData,
-	logout,
+    createUser,
+    login,
+    createRecord,
+    getUserData,
+    logout,
 };
 
 export default api;
